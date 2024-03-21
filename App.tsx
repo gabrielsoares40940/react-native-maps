20eaf8ed0c4874264f5b6f9eee5926f103d93605
Gabriel dos Santos Soares <gustavogusoares14@gmail.com>
2024-03-21 07:05:40 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View,Text } from 'react-native';
+import { styles } from './styles';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchPositionAsync, LocationAccuracy} from 'expo-location';
+import { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps'
+
 
 export default function App() {
+  const [location, setLocation] = useState < LocationObject | null>(null);
+
+  async function requestLocationPermissions() {
+    const {granted} = await requestForegroundPermissionsAsync();
+
+    if (granted){
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+    }
+  }
+
+    useEffect(() =>{
+      requestLocationPermissions();
+    }, []);
+    
+    useEffect(() => {
+      watchPositionAsync({
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1
+      }, (response) => {
+        console.log("NOVA LOCALIZAÇÃO!", response);
+        setLocation(response);
+      });
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.text}>
+        <Text style={styles.text}>
+          TransporChat
+        </Text>
+      </View>
+      {
+        location&&
+
+        <MapView style={styles.map}
+        initialRegion ={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005
+        }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude
+            }}
+            />
+          </MapView>
+      }
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#4387DF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex'
+    },
+  map: {
+    flex: 1,
+    padding: 50,
+    width: '100%'
+  },
+  text: {
+    color: 'black',
+    margin: 15,
+    fontSize: 35,
+    textAlign: 'center',
+    fontStyle: "normal"
+  },
+
+});
